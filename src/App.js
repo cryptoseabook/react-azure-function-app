0db66674f10bf11ffa3a60b102f1d1169a63c011
schen1 <seabook.chen@jemena.com.au>
2019-04-11 16:51:35 +1000

    change theme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="masthead d-flex">
+        <div className="container text-center my-auto">
+          <h1 className="mb-1">幸运大抽奖</h1>
+          <h3 className="mb-5">
+            <em>一个前端ReactJs 后端Servless function 架构的牛逼随机数生成器</em>
+          </h3>
+          <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about">抽字母组</a> 
+          <a className="btn btn-primary btn-xl js-scroll-trigger ml-5" href="#about">抽数字组</a>
+          <div className="result mt-5">
+            <p className="result-letter-group" />
+            <p className="result-number-group" />
+          </div>
+          <div className="footer mt-5">
+            <span style={{color: 'red'}}>♥</span> Built by <a href="https://www.linkedin.com/in/seabookchen/">Seabook Chen</a>@iters.com.au
+          </div>
+        </div>
+      </div>    
     );
   }
 }
